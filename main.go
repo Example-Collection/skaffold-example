3a061fe52060e0c07bd3af695fedc1628c4d605b
MZC01-ROBBYRA <mzc01-robbyra@MZC01-ROBBYRA.local>
2023-01-19 19:29:33 +0900

    feat: Remove /hello endpoint

--- main.go
+++ main.go
@@ -7,9 +7,5 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		_, _ = w.Write([]byte(`{"message": "server is running."}`))
 	})
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		_, _ = w.Write([]byte(`{"message": "Skaffold run on EKS done!"}`))
-	})
 	_ = http.ListenAndServe(":8080", nil)
 }
